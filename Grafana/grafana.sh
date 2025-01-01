ac6c25ee03d7eece8f13985bddcf8aa697272346
Nodateka <122816588+NodatekaII@users.noreply.github.com>
2025-01-01 12:07:14 +0300

    Update grafana.sh

--- Grafana/grafana.sh
+++ Grafana/grafana.sh
@@ -634,8 +634,8 @@ show_link() {
     LINE2="${TERRACOTTA}${BOLD} Login: ${NC}${LIGHT_BLUE}$LOGIN  ${NC} ${TERRACOTTA}${BOLD}Password: ${NC}${LIGHT_BLUE}$PASSWORD${NC}"
    
     # Формируем строки с отступами
-    LINE1_PADDED=$(printf "║ %-*s ║" $((MAX_WIDTH - 4)) "$(echo -e "$LINE1")")
-    LINE2_PADDED=$(printf "║ %-*s ║" $((MAX_WIDTH - 4)) "$(echo -e "$LINE2")")
+    LINE1_PADDED=$(printf "║ %-*s ║" $((MAX_WIDTH - 4)) "$LINE1")
+    LINE2_PADDED=$(printf "║ %-*s ║" $((MAX_WIDTH - 4)) "$LINE2")
 
 
     # Верхняя и нижняя границы
