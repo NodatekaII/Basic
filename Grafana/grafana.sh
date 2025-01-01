14d00530e2becacadb721cc8a367d963ae92e13f
Nodateka <122816588+NodatekaII@users.noreply.github.com>
2025-01-01 10:28:52 +0300

    Update grafana.sh

--- Grafana/grafana.sh
+++ Grafana/grafana.sh
@@ -85,9 +85,9 @@ final_message() {
     echo ""
     show_bold "║ Присоединяйся к Нодатеке, будем ставить ноды вместе!              ║"
     echo ''
-    echo -en "${TERRACOTTA}${BOLD}║ ✈️ Telegram: ${NC}${LIGHT_BLUE}https://t.me/cryptotesemnikov/778${NC}${TERRACOTTA}${BOLD}     ║${NC}\n"
-    echo -en "${TERRACOTTA}${BOLD}║ 🐦 Twitter: ${NC}${LIGHT_BLUE}https://x.com/nodateka${NC}${TERRACOTTA}${BOLD}                       ║${NC}\n"
-    echo -e "${TERRACOTTA}${BOLD}║ ▶️ YouTube: ${NC}${LIGHT_BLUE}https://www.youtube.com/@CryptoTesemnikov${NC}${TERRACOTTA}${BOLD}     ║${NC}\n"
+    echo -en "${TERRACOTTA}${BOLD}║ Telegram: ${NC}${LIGHT_BLUE}https://t.me/cryptotesemnikov/778${NC}${TERRACOTTA}${BOLD}                      ║${NC}\n"
+    echo -en "${TERRACOTTA}${BOLD}║ Twitter: ${NC}${LIGHT_BLUE}https://x.com/nodateka${NC}${TERRACOTTA}${BOLD}                               ║${NC}\n"
+    echo -en "${TERRACOTTA}${BOLD}║ YouTube: ${NC}${LIGHT_BLUE}https://www.youtube.com/@CryptoTesemnikov${NC}${TERRACOTTA}${BOLD}                 ║${NC}\n"
     show_bold '╚═══════════════════════════════════════════════════════════════════╝'
     echo ""
 }
